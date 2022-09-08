a8c4d2cca9fa61c22ff01a32861d38b32462731e
Jarrian <glorianagojar@gmail.com>
2022-09-08 11:56:40 +0800

    update

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const submitBtn = document.querySelector('.input-submit');
 emailFeild.addEventListener('input', () => {
   if(emailFeild.value != "") {
     if(validateEmail(emailFeild)){
+      emailFeild.classList.add('input-field-valid');
       emailFeild.classList.remove('input-field-invalid');
       emailMessage.classList.remove('email-validation-message-show');
       submitBtn.disabled = false;
     } else {
+      emailFeild.classList.remove('input-field-valid');
       emailFeild.classList.add("input-field-invalid");
       emailMessage.classList.add("email-validation-message-show");
       emailMessage.textContent = "Please provide a valid email address";
